3fa314a013006fc5ed6406ffe38dc6c07aa28a0b
aaddula <aaddula@asu.edu>
2023-07-13 20:29:50 -0500

    Added patch operation

--- src/main/java/com/empstatus/controller/EmployeeController.java
+++ src/main/java/com/empstatus/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.empstatus.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,33 +22,24 @@ public class EmployeeController {
 	@Autowired
 	public EmployeeService employeeService;
 	
-	@PostMapping(path="/employees")
+	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
-		
 		return employeeService.createEmployee(employee);
-		
 	}
 	
-	@GetMapping(path="/employees")
-	
-public List<Employee> getEmployees() {
-		
+	@GetMapping("/employees")
+	public List<Employee> getEmployees() {
 		return employeeService.findEmployees();
-		
 	}
-
-	@GetMapping(path = "/employees/{id}")
-
+	
+	@GetMapping("/employees/{id}")
 	public Employee getEmployee(@PathVariable int id) {
-
 		return employeeService.findEmployee(id);
 	}
-
+	
 	@PutMapping("/employees/{id}")
 	public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
-		
 		return employeeService.updateEmployee(id, employee);
-		
 	}
 	
 	@DeleteMapping("/employees/{id}")
@@ -54,6 +47,10 @@ public List<Employee> getEmployees() {
 		employeeService.deleteEmployee(id);
 	}
 	
+	@PatchMapping("/employees/{id}")
+	public Employee updateEmployeeData(@PathVariable int id, @RequestBody Map<String, Object> employee) {
+		return employeeService.patch(id, employee);
+	}
 	
 
 }
--- src/main/java/com/empstatus/service/EmployeeService.java
+++ src/main/java/com/empstatus/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.empstatus.service;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 
 import com.empstatus.model.Employee;
@@ -15,31 +18,43 @@ public class EmployeeService {
 	@Autowired
 	public EmployeeRepository employeeRepository;
 
+	// Post Operation
 	public Employee createEmployee(Employee employee) {
-
 		return employeeRepository.save(employee);
-
 	}
 
+	// Get Operation - get all employees
 	public List<Employee> findEmployees() {
-
 		return employeeRepository.findAll();
-
 	}
 
+	// Get Operation - get employee with an id
 	public Employee findEmployee(int id) {
-		
-		Optional<Employee> temp = employeeRepository.findById(id);
-		return temp.get();
+		return employeeRepository.findById(id).get();
 	}
-	
+
+	// Put Operation
 	public Employee updateEmployee(int id, Employee employee) {
-		//Optional<Employee> temp = employeeRepository.findById(id);
 		employee.setId(id);
 		return employeeRepository.save(employee);
 	}
-	
+
+	// Delete Operation
 	public void deleteEmployee(int id) {
 		employeeRepository.delete(employeeRepository.findById(id).get());
 	}
+
+	// Patch Operation
+	public Employee patch(int id, Map<String, Object> tempEmployee) {
+
+		Optional<Employee> employee = employeeRepository.findById(id);
+		tempEmployee.forEach((key, value) -> {
+			Field field = ReflectionUtils.findField(Employee.class, key);
+			ReflectionUtils.makeAccessible(field);
+			ReflectionUtils.setField(field, employee.get(), value);
+		});
+
+		return employeeRepository.save(employee.get());
+	}
+
 }
