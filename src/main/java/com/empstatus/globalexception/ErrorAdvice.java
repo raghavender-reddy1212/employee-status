01b065e808e630bd8a4a9bd38517c9ff960087f8
raghavender-reddy1212 <45497921+raghavender-reddy1212@users.noreply.github.com>
2023-07-15 15:52:03 -0500

    added profiles to the apis
    added profiles

--- src/main/java/com/empstatus/globalexception/ErrorAdvice.java
+++ src/main/java/com/empstatus/globalexception/ErrorAdvice.java
@@ -3,18 +3,23 @@ package com.empstatus.globalexception;
 import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.empstatus.utility.Constants;
+
 @ControllerAdvice
 public class ErrorAdvice extends ResponseEntityExceptionHandler{
 	
+	@Autowired
+	public Constants constants;
 	@ExceptionHandler
 	public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ne) {
-		ErrorResponse errorResponse=new ErrorResponse("400",HttpStatus.BAD_REQUEST,ne.getMessage(),LocalDateTime.now());
+		ErrorResponse errorResponse=new ErrorResponse(constants.getStatusCode400(),HttpStatus.BAD_REQUEST,ne.getMessage(),LocalDateTime.now());
 		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.BAD_REQUEST);
 		
 	}
